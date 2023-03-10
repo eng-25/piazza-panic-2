301ab343bce422772eefbf2efa661855974b61e0
Faran <fjl515@york.ac.uk>
2023-03-10 14:07:50 +0000

    Added Tests directory

---
+++ tests/src/main/tests/AssetTests.java
@@ -0,0 +1,17 @@
+package main.tests;
+
+import com.badlogic.gdx.Gdx;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(GdxTestRunner.class)
+public class AssetTests {
+
+    @Test
+    public void testLettuceChoppedAssetExists() {
+        assertTrue("This test will only pass when the lettuce_chopped.png asset exists.", Gdx.files
+                .internal("food/original/lettuce_chopped.png").exists());
+    }
+}
