b9d7438e971e67d5df15ea22d97f296b01e9eef0
Samuel Nicholson <sn1155@york.ac.uk>
2023-03-14 18:57:19 +0000

    Start of writing testing classes + commenting classes to help understanding

---
+++ tests/src/main/tests/StationTests.java
@@ -0,0 +1,23 @@
+package main.tests;
+
+import cs.eng1.piazzapanic.food.FoodTextureManager;
+import cs.eng1.piazzapanic.food.ingredients.Bun;
+import cs.eng1.piazzapanic.food.ingredients.Ingredient;
+import cs.eng1.piazzapanic.food.FoodTextureManager;
+import cs.eng1.piazzapanic.food.ingredients.Lettuce;
+import cs.eng1.piazzapanic.stations.ChoppingStation;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(GdxTestRunner.class)
+public class StationTests {
+
+    @Test
+    public void testIsCorrectIngredient() {
+        FoodTextureManager FoodTextureManager = new FoodTextureManager();
+        Lettuce lettuce = new Lettuce(FoodTextureManager);
+        ChoppingStation choppingStation = new ChoppingStation();
+
+    }
+
+}
